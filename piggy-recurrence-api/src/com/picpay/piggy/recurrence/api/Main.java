1980a0e51d223013262d9c77f9a8f64f76c1a874
Everson Gouvea <everson.araujo@picpay.com>
2023-07-31 11:36:49 -0300

    feat: modularização para api, batch e common

---
+++ piggy-recurrence-api/src/com/picpay/piggy/recurrence/api/Main.java
@@ -0,0 +1,15 @@
+package com.picpay.piggy.recurrence.api;
+
+/**
+ * Class comments go here...
+ *
+ * @author everson.araujo
+ * @version 1.0 $DAY/$MONTH/$YEAR
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
